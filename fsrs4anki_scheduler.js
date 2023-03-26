e5861af66872568d752655b7bfc1d94ae131a20e
banbeucmas <banbeucams@gmail.com>
2023-03-26 23:38:26 +0900

    Custom parameter

--- fsrs4anki_scheduler.js
+++ fsrs4anki_scheduler.js
@@ -8,7 +8,7 @@ const deckParams = [
   {
     // Default parameters of FSRS4Anki for global
     "deckName": "global config for FSRS4Anki",
-    "w": [1, 1, 5, -0.5, -0.5, 0.2, 1.4, -0.12, 0.8, 2, -0.2, 0.2, 1],
+    "w": [0.8129, 1.0977, 5.1345, -0.7019, -0.7107, 0.0366, 1.392, -0.01, 0.7891, 1.676, -0.5198, 0.2923, 0.6916],
     // The above parameters can be optimized via FSRS4Anki optimizer.
     // For details about the parameters, please see: https://github.com/open-spaced-repetition/fsrs4anki/wiki/Free-Spaced-Repetition-Scheduler
     // User's custom parameters for global
@@ -19,32 +19,12 @@ const deckParams = [
     // FSRS only modifies the long-term scheduling. So (re)learning steps in deck options work as usual.
     // I recommend setting steps shorter than 1 day.
   },
-  {
-    // Example 1: User's custom parameters for this deck and its sub-decks.
-    // Need to add <div id=deck deck_name="{{Deck}}"></div> to your card's front template's first line.
-    "deckName": "ALL::Learning::English::Reading",
-    "w": [1.1475, 1.401, 5.1483, -1.4221, -1.2282, 0.035, 1.4668, -0.1286, 0.7539, 1.9671, -0.2307, 0.32, 0.9451],
-    "requestRetention": 0.9,
-    "maximumInterval": 36500,
-    "easyBonus": 1.3,
-    "hardInterval": 1.2,
-  },
-  {
-    // Example 2: User's custom parameters for this deck and its sub-decks.
-    // Don't omit any keys.
-    "deckName": "ALL::Archive",
-    "w": [1.2879, 0.5135, 4.9532, -1.502, -1.0922, 0.0081, 1.3771, -0.0294, 0.6718, 1.8335, -0.4066, 0.7291, 0.5517],
-    "requestRetention": 0.9,
-    "maximumInterval": 36500,
-    "easyBonus": 1.3,
-    "hardInterval": 1.2,
-  }
 ];
 
 // To turn off FSRS in specific decks, fill them into the skip_decks list below.
 // And add <div id=deck deck_name="{{Deck}}"></div> to your card's front template's first line.
 // Please don't remove it even if you don't need it.
-const skip_decks = ["ALL::Learning::ML::NNDL", "ALL::Learning::English"];
+const skip_decks = [];
 
 // "Fuzz" is a small random delay applied to new intervals to prevent cards from
 // sticking together and always coming up for review on the same day
